61ceffeaf6b6dfd244f6bea095fc7310f7499828
Yemi Oduyemi <yhermii@gmail.com>
2025-12-30 02:46:05 +0100

    updated meta desc

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "ProGrowing",
-  description: "Your go-to fashion plug",
+  description: "Mentorship community",
 };
 
 export default function RootLayout({ children }) {
